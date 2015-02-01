52cc1fcb4700e2ce307d2faf6c349f04f4e319d7
Jia <zhangjia@mit.edu>
2015-02-01 18:35:40 -0500

    restructured data, working, need to add value, need to add timeline

--- dashboard_heatmap.js
+++ dashboard_heatmap.js
@@ -1,5 +1,13 @@
 var config = {
-	timer:null
+	timeline:{
+		timer:null,
+		width:1000,
+		margin:20,
+		xScale:d3.scale.linear().domain([0,180]).range([20,980])
+	}
+}
+var global = {
+	data:null
 }
 
 $(function() {
@@ -18,66 +26,147 @@ var heatmapInstance = h337.create({
     gradient: {
        // enter n keys between 0 and 1 here
        // for gradient color customization
-       '.5': '#fff',
-       '.8': '#aaa',
-       '.95': '#000'
+       '.2': '#eee',
+       '.95': '#aaa',
+       '1': '#000'
      },
-	 maxOpacity: 1,
-	 minOpacity: 1
+	 maxOpacity: .9,
+	 minOpacity: .1
   });
-
-function drawTimeline(data){
-	
-}
-//draw timeline range
-//draw handles
-//set handles to inital interval
-//animate 
 
 function dataDidLoad(error,data1,data2,data3,data4) {
 	//make new data object and add different files(paths) to it
 	$("#timeline-controls .stop").hide()
-	
-	var allInterval = data1.values.length
+
+	var allInterval = data1.values.length-1
 	var allData = {}
-	allData = formatData(data1,allData,allInterval)
-	 heatmapInstance.setData(allData[1]);
+	allInOneInterval = formatData(data1,allData,data1.values.length-1)
+	allInOneInterval = formatData(data2,allData,data2.values.length-1)
+	allInOneInterval = formatData(data3,allData,data3.values.length-1)
 	
-	
-	
+	//console.log(allData)
+	heatmapInstance.setData(allInOneInterval[0]);
+
+
 	var newData = {}
-	var interval = 50
-	data =formatData(data1,newData,interval)
-	data =formatData(data2,newData,interval)
-	data =formatData(data3,newData,interval)
-	data =formatData(data4,newData,interval)
+	var interval = 1000
+	var start = 0
+	var end = 1800
+		data =formatData(data1,newData,interval)
+		data =formatData(data2,newData,interval)
+		data =formatData(data3,newData,interval)
+	//data =formatData(data4,newData,interval)
 	animate(data)
 }
 
-function filterDataAndAnimate(data,start,end){
-	
+
+var utils = {
+range: function(start, end) {
+	var data = []
+
+	for (var i = start; i < end; i++) {
+		data.push(i)
+	}
+
+	return data
 }
+}
+
+var table = {
+	group: function(rows, fields) {
+		var view = {}
+		var pointer = null
+
+		for(var i in rows) {
+			var row = rows[i]
+
+			pointer = view
+			for(var j = 0; j < fields.length; j++) {
+				var field = fields[j]
+
+				if(!pointer[row[field]]) {
+					if(j == fields.length - 1) {
+						pointer[row[field]] = []
+					} else {
+						pointer[row[field]] = {}
+					}
+				}
+
+				pointer = pointer[row[field]]
+			}
+
+			pointer.push(row)
+		}
+
+		return view
+	},
+
+	filter: function(view, callback) {
+		var data = []
+
+		for(var i in view) {
+			var list = view[i]
+			if(callback(list, i)) {
+				data = data.concat(list)
+			}
+		}
+
+		return data
+	}
+}
+
 
 function formatData(data, newData,interval){
 	var interval = interval
-	var frame = 0
+	var totalLength = data.values.length
+	//console.log(totalLength)
+	var data = table.group(data.values,["frame"])
+	//console.log(data)
+	for(var frame = 0; frame + interval < totalLength; frame++){
 		
-	jQuery.each(data.values,function(i,d){
-		d["min"] = 0
-		d["max"] = 100
-		if(newData[frame] == undefined){
-			newData[frame]={}
-			newData[frame]["data"] = []
-			newData[frame]["data"].push({"x":d.x, "y":d.y, "max":10,"min":0})
+		if(newData[frame]==undefined){
+			newData[frame] = {}
+			newData[frame]["data"]=[]
+			//console.log(newData)
+			var existingCoordinates = []
+			for(var i =1; i < interval; i++){
+				var currentFrame = parseInt(frame)+i
+				//console.log(data[20])
+				var x = data[currentFrame][0].x
+				var y = data[currentFrame][0].y
+				
+				newData[frame]["data"].push({"x":x, "y":y})
+			}
 		}else{
-			newData[frame]["data"].push({"x":d.x, "y":d.y,"max":10,"min":0})
+			for(var i =1; i < interval; i++){
+				var currentFrame = parseInt(frame)+i
+				//console.log(data[20])
+				var x = data[currentFrame][0].x
+				var y = data[currentFrame][0].y
+				newData[frame]["data"].push({"x":x, "y":y})
+			}
 		}
-		if(i%interval == 0){
-			frame +=1
-		}
-		d["data"] = [{"x":d.x, "y":d.y,"value":d.value}]
-		//console.log(d)
-	})
+	}
+	//console.log(newData)
+//	jQuery.each(data,function(i,d){
+//			d["min"] = 0
+//			d["max"] = 100
+//			if(newData[frame] == undefined){
+//				newData[frame]={}
+//				newData[frame]["data"] = []
+//				newData[frame]["data"].push({"x":d.x, "y":d.y, "max":10,"min":0})
+//			}else{
+//				newData[frame]["data"].push({"x":d.x, "y":d.y,"max":10,"min":0})
+//			}
+//			
+//			if(i%interval == 0){
+//				frame +=1
+//			}
+//			
+//			d["data"] = [{"x":d.x, "y":d.y,"value":d.value}]
+//			//console.log(d)
+//	
+//	})
 //	console.log(newData)
 	return newData
 }
@@ -111,7 +200,7 @@ function animate(data) {
 				 timelineControlStop()
 			 }
 			 
-		 }, 200);
+		 }, 3);
 		 
 		 if(i == size){
 			 timelineControlStop
